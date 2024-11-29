71edf29ed6c02c599a996f8ca802feecd369737b
Omar Baig <120315102+OmarBaig05@users.noreply.github.com>
2024-11-30 01:49:08 +0500

    Updates

---
+++ component---src-pages-index-js-2e3794ad0c6ad40a4af7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{6558:function(e,t,a){a.r(t);var l=a(7294),u=a(4160);t.default=()=>l.createElement("main",null,l.createElement("h1",null,"Welcome to Gatsby"),l.createElement(u.rU,{to:"/about"},"Go to About Page"))}}]);
+//# sourceMappingURL=component---src-pages-index-js-2e3794ad0c6ad40a4af7.js.map
